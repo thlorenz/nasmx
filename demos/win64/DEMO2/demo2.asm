53c3fe8d24d86bcca5f51671860a130d1f841ad7
p1ranha-rob <p1ranha-rob@940d910e-ec9b-4887-81ff-7f9004fb9324>
2011-01-23 05:24:11 +0000

    Add 64 bit float demo

--- demos/win64/DEMO2/demo2.asm
+++ demos/win64/DEMO2/demo2.asm
@@ -1,5 +1,4 @@
 %include "..\..\windemos.inc"
-%include "..\..\..\inc\win32\msvcrt.inc"
 
 DEFAULT REL
 
@@ -8,39 +7,40 @@ entry	demo2
 [section .bss]
 	fname:	resb 1025
 	lname:	resb 1025
-	age:	resq 1
+;	age:	resq 1
 
 [section .data]
-    szFirstName: declare(NASMX_TCHAR) NASMX_TEXT("First Name: "), 0x0
-    szLastName:  declare(NASMX_TCHAR) NASMX_TEXT("Last Name: "), 0x0
-    szAge:       declare(NASMX_TCHAR) NASMX_TEXT("Age: "), 0x0
-	szHello:     declare(NASMX_TCHAR) NASMX_TEXT("Hello, %s %s! What are you having today?"), 0x0
-	szSorry:     declare(NASMX_TCHAR) NASMX_TEXT("Sorry kid, you're only %d!"), 0x0
-	szScanStr:   declare(NASMX_TCHAR) NASMX_TEXT("%1024s"), 0x0
-	szScanNum:   declare(NASMX_TCHAR) NASMX_TEXT("%02d"), 0x0
-	szPressKey:  declare(NASMX_TCHAR) NASMX_TEXT("%c%c%c%cPress any key to exit."),13,10,13,10, 0x0
+    szFirstName: declare(NASMX_TCHAR) NASMX_TEXT("First Name: "), 0
+    szLastName:  declare(NASMX_TCHAR) NASMX_TEXT("Last Name: "), 0
+    szAge:       declare(NASMX_TCHAR) NASMX_TEXT("Age: "), 0
+	szHello:     declare(NASMX_TCHAR) 13,10,NASMX_TEXT("Hello, %s %s! What are you having today?"), 13,10,0
+	szSorry:     declare(NASMX_TCHAR) 13,10,NASMX_TEXT("Sorry kid, you're only %d!"), 13,10,0
+	szScanStr:   declare(NASMX_TCHAR) NASMX_TEXT("%1024s"), 0
+	szScanNum:   declare(NASMX_TCHAR) NASMX_TEXT("%02d"), 0
+	szPressKey:  declare(NASMX_TCHAR) 13,10,13,10,NASMX_TEXT("Press any key to exit."),13,10, 0
 
 [section .text]
 
 NASMX_PRAGMA CALLSTACK, 64
 
 proc   demo2
-locals none
+locals
+    local age, qword
+endlocals
 
 	invoke	printf, szFirstName
 	invoke	scanf, szScanStr, fname
 	invoke	printf, szLastName
 	invoke	scanf, szScanStr, lname
 	invoke	printf, szAge
-	invoke	scanf, szScanNum, age
-	mov	rax, qword[age]
+	invoke	scanf, szScanNum, var(.age)
+	mov	rax, qword [var(.age)]
 	if	rax, >=, 21
 		invoke	printf, szHello, fname, lname
 	else
-		invoke	printf, szSorry, qword[age]
+		invoke	printf, szSorry, qword [var(.age)]
 	endif
-	invoke	printf, szPressKey
-	invoke	_getch
+
 	xor	rax, rax
 
 endproc
