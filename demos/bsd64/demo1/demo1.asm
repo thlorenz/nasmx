3c6733bdda50236dc8c2625e5de3a559f5a57d7c
p1ranha-rob <p1ranha-rob@940d910e-ec9b-4887-81ff-7f9004fb9324>
2012-10-27 21:35:50 +0000

    Added BSD 64-bit demos

---
+++ demos/bsd64/demo1/demo1.asm
@@ -0,0 +1,38 @@
+;// demo1.asm
+;//
+;// Copyright (C)2005-2012 The NASMX Project
+;//
+;// Purpose:
+;//    This program demonstrates basic usage of the NASMX
+;//    macros and how to use the framework.
+;//
+;// Contributors:
+;//    Bryant Keller
+;//    Rob Neff
+;//
+
+;##### Include Files #####
+%include 'nasmx.inc'
+%include 'bsd/libc.inc'
+
+entry	demo1
+
+;##### Initialized Data #####
+[section .data]
+msg		DB	10,"Hello, World!!!",10,10,0
+msglen		equ	($ - msg)
+
+[section .text]
+
+;##### Program Entrypoint #####
+proc   demo1, ptrdiff_t count, ptrdiff_t cmdline
+locals none
+
+    ;#### SYSCALL Write Message to the Console ####
+    syscall write, STDOUT_FILENO, msg, msglen
+
+    ;#### Exit with NULL Return Value ####
+    syscall exit, 0
+
+endproc
+
