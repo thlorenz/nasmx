a93bb7923ffda9f40c6d7d5b4e60bb053cf06b81
p1ranha-rob <p1ranha-rob@940d910e-ec9b-4887-81ff-7f9004fb9324>
2014-02-14 17:30:33 +0000

    undo bad checkin

--- tags/nasmx-1.3/demos/win32/DEMO17/demo17.bat
+++
@@ -1,2 +0,0 @@
-nasm -fwin32 demo17.asm
-golink /entry _main kernel32.dll user32.dll gdi32.dll demo17.obj
--- tags/nasmx-1.3/demos/win64/DEMO6/demo6.bat
+++
@@ -1,2 +0,0 @@
-nasm -fwin64 demo6.asm
-golink /entry main kernel32.dll user32.dll gdi32.dll demo6.obj
