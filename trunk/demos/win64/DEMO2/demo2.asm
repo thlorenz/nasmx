4d04163d76fd9e3882eab2ae4891633d04d31873
p1ranha-rob <p1ranha-rob@940d910e-ec9b-4887-81ff-7f9004fb9324>
2014-03-05 19:05:51 +0000

    give old grandpa a free drink

--- trunk/demos/win64/DEMO2/demo2.asm
+++ trunk/demos/win64/DEMO2/demo2.asm
@@ -1,6 +1,6 @@
 ;// DEMO2.ASM
 ;//
-;// Copyright (C)2005-2011 The NASMX Project
+;// Copyright (C)2005-2014 The NASMX Project
 ;//
 ;// This is a fully UNICODE aware, typedefined demo that demonstrates
 ;// using NASMX typedef system to make your code truly portable between
@@ -20,17 +20,16 @@ entry	demo2
 [section .bss]
 	fname:	resb 1025
 	lname:	resb 1025
-;	age:	resq 1
 
 [section .data]
     szFirstName: declare(NASMX_TCHAR) NASMX_TEXT("First Name: "), 0
     szLastName:  declare(NASMX_TCHAR) NASMX_TEXT("Last Name: "), 0
     szAge:       declare(NASMX_TCHAR) NASMX_TEXT("Age: "), 0
-	szHello:     declare(NASMX_TCHAR) 13,10,NASMX_TEXT("Hello, %s %s! What are you having today?"), 13,10,0
-	szSorry:     declare(NASMX_TCHAR) 13,10,NASMX_TEXT("Sorry kid, you're only %d!"), 13,10,0
-	szScanStr:   declare(NASMX_TCHAR) NASMX_TEXT("%1024s"), 0
-	szScanNum:   declare(NASMX_TCHAR) NASMX_TEXT("%02d"), 0
-	szPressKey:  declare(NASMX_TCHAR) 13,10,13,10,NASMX_TEXT("Press any key to exit."),13,10, 0
+    szHello:     declare(NASMX_TCHAR) 13,10,NASMX_TEXT("Hello, %s %s! What are you having today?"), 13,10,0
+    szSorry:     declare(NASMX_TCHAR) 13,10,NASMX_TEXT("Sorry kid, you're only %d!"), 13,10,0
+    szFreeDrink: declare(NASMX_TCHAR) 13,10,NASMX_TEXT("Good lord, %s %s!  At your age please accept a free drink on the house!"), 13,10,0
+    szScanStr:   declare(NASMX_TCHAR) NASMX_TEXT("%1024s"), 0
+    szScanNum:   declare(NASMX_TCHAR) NASMX_TEXT("%d"), 0
 
 [section .text]
 
@@ -48,7 +47,9 @@ endlocals
 	invoke	printf, szAge
 	invoke	scanf, szScanNum, var(.age)
 	mov	rax, qword [var(.age)]
-	if	rax, >=, 21
+	if	rax, >=, 90
+		invoke	printf, szFreeDrink, fname, lname
+	elsif	rax, >=, 21
 		invoke	printf, szHello, fname, lname
 	else
 		invoke	printf, szSorry, qword [var(.age)]
